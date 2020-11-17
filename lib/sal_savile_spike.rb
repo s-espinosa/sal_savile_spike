9bea38d177f9ee334ba7ac91d158b38e1f60f7d7
Sal Espinosa <sespinos@gmail.com>
2020-11-17 09:41:09 -0700

    Add button styling and path manipulation for Rails/Sass

--- lib/sal_savile_spike.rb
+++ lib/sal_savile_spike.rb
@@ -2,5 +2,16 @@ require "sal_savile_spike/version"
 
 module SalSavileSpike
   class Error < StandardError; end
-  # Your code goes here...
+
+  if defined?(Rails) && defined?(Rails::Engine)
+    class Engine < ::Rails::Engine
+      config.assets.paths << File.expand_path("../core", __dir__)
+    end
+  else
+    begin
+      require "sass"
+      Sass.load_paths << File.expand_path("../core", __dir__)
+    rescue LoadError
+    end
+  end
 end
